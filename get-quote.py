4ec36e3e839f7425b02d37f85a9be77e09ae9ac1
Gilles <gilles@Mini-de-Gilles.local>
2020-09-01 20:55:58 +0200

    Programme complet

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,33 @@
-import random
-
-def primary():
-  #print("Keep it logically awesome.")
-
-  f = open("quotes.txt")
+def main():
+  # Ajout d'une ligne
+  f = open("quotes.txt","a")
+  f.write("ligne supplémentaire 5")
+  f.close()
+  
+  #Lecture et affichage des lignes
+  f = open("quotes.txt","r")
   quotes = f.readlines()
   f.close()
 
-  last = len(quotes) -1
-  rnd = random.randint(0,last)
-
-  print(quotes[rnd])
+  last = len(quotes) - 1
+  
+  for i in range(0,last + 1): 
+    print(quotes[i])
+  
+  print ("*******************************************************")
+  
+  # Suppression de la dernière ligne écrite
+  derniere_ligne = quotes[i]
+  quotes.remove(derniere_ligne)
+   
+  # Ecriture du fichier et affichge 
+  last = len(quotes) - 1
+  
+  f = open("quotes.txt","w")
+  for i in range(0,last + 1):
+    f.write(quotes[i])
+    print(quotes[i])
+  f.close()
 
 if __name__== "__main__":
-  primary()
+  main()
